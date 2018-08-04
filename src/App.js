12f455457b9ddff09df3112dfa669ef5dd40f97a
austinChappell <Austin.W.Chappell@gmail.com>
2018-08-03 23:32:41 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-custom-date-picker';
+import moment from 'moment';
 
 import './App.css';
 
 class App extends Component {
   state = {
-    date: new Date(2018, 11, 12),
+    date: null,
+    // endDate: null,
   }
 
   handleDateChange = (date) => {
     this.setState({ date });
   }
 
+  // handleDateChange = ([date, endDate]) => {
+  //   this.setState({
+  //     date,
+  //     endDate,
+  //   });
+  // }
+
   render() {
     return (
       <DatePicker
+        color="#296b3e"
         date={this.state.date}
+        endDate={this.state.endDate}
+        errorColor="#c32c27"
         handleDateChange={this.handleDateChange}
+        // hoverWeek
+        inputStyle={{
+          borderRadius: 0
+        }}
+        lightHeader
+        modal
+        // range
+        required
       />
     );
   }
